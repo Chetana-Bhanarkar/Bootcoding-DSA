04f9c627597e352cc7fbf9e8ada96df1ed98f671
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2022-12-15 15:31:42 +0530

    Binary Tree Inorder Traversing

---
+++ Binary Tree/InOrder1.js
@@ -0,0 +1,37 @@
+//inorder binary tree :-  L - ROOT - R 
+
+
+class Node{
+    constructor(data,left,right){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+let root = new Node('A');
+let b = new Node('B');
+let c = new Node('C');
+let p = new Node('P');
+let q = new Node("Q");
+let r = new Node("R");
+let s = new Node("S");
+
+
+root.left = b;
+root.left.left = p;
+root.left.right = q;
+root.right = c;
+root.right.left = r;
+root.right.right = s;
+
+
+function InOrder(root){
+    if(root==null){
+        return
+    }
+    InOrder(root.left);
+    console.log(`${root.data} -> `);
+    InOrder(root.right);
+}
+
+InOrder(root);
