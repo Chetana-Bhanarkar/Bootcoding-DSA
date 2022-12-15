c4cea51bd60f7d65b3c47abaf8b0da773ed664de
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2022-12-15 16:12:41 +0530

    Binary tree PreOrder Traversing

---
+++ Binary Tree/PreOrder.js
@@ -0,0 +1,37 @@
+//preOrder binary tree :-  Root - L - R
+
+
+class Node{
+    constructor(data,left,right){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+let root = new Node('A');
+let b = new Node('B');
+let c = new Node('C');
+let p = new Node('P');
+let q = new Node("Q");
+let r = new Node("R");
+let s = new Node("S");
+
+
+root.left = b;
+root.left.left = p;
+root.left.right = q;
+root.right = c;
+root.right.left = r;
+root.right.right = s;
+
+
+function preOrder(root){
+    if(root==null){
+        return
+    }
+    console.log(`${root.data} -> `);
+    preOrder(root.left);
+    preOrder(root.right);
+}
+
+preOrder(root);
