b4661d6e6da9e86bd300a50c56ced458921d1f76
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2022-12-21 16:47:23 +0530

    Concat two linkedlist

---
+++ linkedlist-practice/concatLinkedList.js
@@ -0,0 +1,40 @@
+class Node{
+    constructor(data){
+    this.data =  data ; 
+    this.next = null;
+    }
+}
+
+let head1 = new Node(10);
+let second = new Node(20);
+
+
+head1.next = second;
+
+
+let head2 = new Node(100);
+let second2 = new Node(200);
+
+head2.next =  second2
+
+const printlist = (head1) => {
+    let temp1 = head1 ; 
+    while(temp1 != null){
+        console.log(temp1.data);
+        temp1 = temp1.next;
+    }
+
+}
+
+const concat = (head1,head2) =>{
+    let temp1 = head1;
+    let temp2 = head2;
+    while(temp1.next != null){
+        // console.log(temp1.data);
+        temp1 = temp1.next
+    };
+    temp1.next = temp2;
+    // printlist(head1);
+}
+
+concat(head1,head2);
