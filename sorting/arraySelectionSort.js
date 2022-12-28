ea13f34638a0d359684d636d509d67778c693e21
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2022-12-28 12:42:38 +0530

    Sorting array using selection sort alogrithm

---
+++ sorting/arraySelectionSort.js
@@ -0,0 +1,18 @@
+const array = [8,9,6,3,2];
+
+const selectionSort = (a) => {
+    for(let i=0; i<a.length; i++){
+        let lowest = i;
+        for(let j=i+1 ; j<a.length ; j++){
+            if(a[j]<a[i]){
+                lowest = j;
+            }
+            if(lowest != i){
+                [a[j],a[i]] = [a[i],a[j]]
+            }
+        }
+    }
+    return a;
+}
+
+console.log(selectionSort(array));
