0be865616946af6281ec8a62ce94be841e84ee33
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2022-12-28 15:14:12 +0530

    sorting an array using bubble sort

--- sorting/arrayBubbleSort.js
+++ sorting/arrayBubbleSort.js
@@ -1,18 +1,13 @@
-let array = [100,80,90,70,60,50,40,20,30,10];
 
-const bubbleSort = (a) =>{
-    for(let i=0;i<a.length;i++){
-        lowest = i; //100
-        for(let j=i+1;j<a.length;j++){
-            if(a[j]<a[lowest]){
-                lowest = j;
+
+const bubbleSort = (a) => {
+    for (let i = a.length; i > 0; i--) {
+        for (let j = 0; j < i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                [a[j], a[j + 1]] = [a[j + 1], a[j]];
             }
-        }
-        if(lowest != i){
-            [a[i],a[lowest]] = [a[lowest],a[i]];
         }
     }
     return a;
 }
-
-console.log(bubbleSort(array));
+console.log(bubbleSort([100, 80, 90, 70, 60, 50, 40, 20, 30, 10]))
