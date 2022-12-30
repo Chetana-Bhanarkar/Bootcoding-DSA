e621e2723a28b05e852aa13c8dc2ec124ed6e373
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2022-12-30 12:27:06 +0530

    Sorting an array using insertion sort

---
+++ sorting/arrayInsertionSort.js
@@ -0,0 +1,15 @@
+let array = [2, 4, 1, 8, 3, 0, 6, 9];
+
+const insertionSort = (a) => {
+    for (let i = 1; i < a.length; i++) {
+        let currentValue = a[i];
+        let j;
+        for (j = i - 1; j >= 0 && a[j] > currentValue; j--) {
+            a[j + 1] = a[j]
+        }
+        a[j + 1] = currentValue;
+    }
+    return a;
+}
+
+console.log(insertionSort(array));
