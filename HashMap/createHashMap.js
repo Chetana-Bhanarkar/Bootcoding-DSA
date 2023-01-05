2db413e82c4e063b0e01b0a62ea95a0d15f40db3
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-01-05 17:00:41 +0530

    Create hash map

---
+++ HashMap/createHashMap.js
@@ -0,0 +1,21 @@
+let a = new Map();
+let b = new Map([["apple",80],[2,"second"],[60,5]]);
+
+// for(let element of b){
+//     console.log(element);
+// }
+a.set(10,100);
+a.set(20,200);
+a.set(30,300);
+a.set("apple",1);
+
+// for(let m of a){
+//     console.log(m);
+// }
+
+// console.log(a.get(10));
+// console.log(a.has(20));
+// console.log(a.size);
+
+console.log(b.keys());
+console.log(b.values());
