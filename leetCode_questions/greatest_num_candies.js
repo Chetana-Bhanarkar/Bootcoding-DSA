3a9d063a3537ac039b1fe54acdf58e2c7dfd4b13
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-01-25 11:29:13 +0530

    Kids With the Greatest Number of Candies

---
+++ leetCode_questions/greatest_num_candies.js
@@ -0,0 +1,27 @@
+// Input: candies = [2,3,5,1,3], extraCandies = 3
+// Output: [true,true,true,false,true] 
+// Explanation: If you give all extraCandies to:
+// - Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+// - Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+// - Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+// - Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+// - Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+
+
+var kidsWithCandies = function (candies, extraCandies) {
+    let a = Math.max(...candies);
+    let b = [];
+  
+    for (let i = 0; i < candies.length; i++) {
+      if (candies[i] + extraCandies >= a) {
+        b.push(true);
+      } else {
+        b.push(false);
+      }
+    }
+  
+    return b;
+  };
+
+
+  console.log(kidsWithCandies([2,3,5,1,3]));
