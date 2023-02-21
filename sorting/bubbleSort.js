7e505a4b67bdaa78412e93a30d3c80764dcf1171
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-02-21 12:01:53 +0530

    bubble sort

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,4 +1,7 @@
-let array = [7,3,4,8,5,9] //output : [3,4,5,7,8,9]
+// let array = [7,3,4,8,5,9] //output : [3,4,5,7,8,9]
+
+
+let array = [4,3,2,5,6,7,1] //output : [1,2,3,4,5,6,7]
 
 const bubbleSort = (a) => {
     for(let i=a.length;i>0;i--){
