bee306cbc2b1dd5d626b528b480e26363e00d9ed
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-04-14 14:29:35 +0530

    258 Add Digits

---
+++ leetCode_questions/Add_digits.js
@@ -0,0 +1,15 @@
+var addDigits = function(n) {
+    let sum = 0;
+    while (n > 0 || sum > 9) {
+         if(n == 0) {
+            n = sum;
+            sum = 0;
+         }
+         sum = sum + n % 10;
+         n = Math.floor(n / 10);
+    }
+    return sum;
+
+};
+
+console.log(addDigits(4));
