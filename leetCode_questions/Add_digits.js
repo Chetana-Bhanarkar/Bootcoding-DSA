be1a3cf2b39df0bea05d0397647e73f6e29408d0
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-04-17 23:37:09 +0530

    add digits

--- leetCode_questions/Add_digits.js
+++ leetCode_questions/Add_digits.js
@@ -1,15 +1,16 @@
 var addDigits = function(n) {
-    let sum = 0;
-    while (n > 0 || sum > 9) {
-         if(n == 0) {
-            n = sum;
-            sum = 0;
-         }
-         sum = sum + n % 10;
-         n = Math.floor(n / 10);
+    if(n == 0){
+        return 0 ;
     }
-    return sum;
+    else if(n%9 =){
+        return n ; 
+    }
+    else{
+        n = n%9 ; 
+        return n
+    }
+    
 
 };
 
-console.log(addDigits(4));
+console.log(addDigits(18));
