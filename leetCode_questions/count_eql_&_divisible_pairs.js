cd5e41c2afbdb91fcb4053450f9c5ee14feb651a
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-04-18 23:40:04 +0530

    count equal and divisible by pairs

--- leetCode_questions/count_eql_&_divisible_pairs.js
+++ leetCode_questions/count_eql_&_divisible_pairs.js
@@ -1,11 +1,15 @@
 let nums = [1,2,3,4], k = 1
 
 
-var countPairs = function(nums, k) {
+var countPairs = function(nums, k) 
+{
     let count = 0;
-    for(let i=0 ; i<nums.length;i++){
-        for(j=i+1 ; j<nums.length ; j++){
-            if((nums[i] === nums[j]) && ((i*j)%k == 0)){
+    for(let i=0 ; i<nums.length;i++)
+    {
+        for(j=i+1 ; j<nums.length ; j++)
+        {
+            if((nums[i] === nums[j]) && ((i*j)%k == 0))
+            {
                 count++;
             }
         }
