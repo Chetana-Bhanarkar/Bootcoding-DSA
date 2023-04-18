824bf98ca8c606ef84831bfe67002ff020fd08c4
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-04-18 23:38:00 +0530

    sum of unique elements

--- leetCode_questions/Sum_of_unique_elements.js
+++ leetCode_questions/Sum_of_unique_elements.js
@@ -2,19 +2,34 @@
 // Output: 4
 // Explanation: The unique elements are [1,3], and the sum is 4.
 
-let nums = [1,2,3,2]
-var sumOfUnique = function(nums) {
+let nums = [1, 2, 3, 2]
+// var sumOfUnique = function(nums) {
+//     let sum = 0;
+//     for(let i=0 ; i<nums.length;i++){
+//         for(let j=1;j<=nums.length;j++){
+//             if(nums[i] == nums[j]){
+//                 break;
+//             }
+//         }
+//         sum += nums[i];
+
+//         }
+//         return sum;
+// };
+
+
+let value = [];
+var sumOfUnique = function (nums) {
     let sum = 0;
-    for(let i=0 ; i<nums.length;i++){
-        for(let j=i+1;j<=nums.length;j++){
-            if(nums[i] == nums[j]){
+    for (let i = 0; i <= nums; i++){
+        for (let j = 1; j<= nums.length; j++) {
+            if (nums[i] == nums[j]){
                 break;
-            }else{
-                sum += nums[i];
             }
         }
     }
-    return sum;
-};
+    return sum
+}
+
 
 console.log(sumOfUnique(nums));
