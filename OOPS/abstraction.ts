cf398b158fadee75922c36a65134f3bc05df32ae
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-05-02 13:03:20 +0530

    abstraction in typescript

--- OOPS/abstraction.ts
+++ OOPS/abstraction.ts
@@ -1,9 +1,18 @@
-abstract class student{
+abstract class persons{
     name : string ; 
     constuctor(name : string){
         this.name = name ; 
     }
+}
 
+
+class person extends persons{
+    age : number ; 
+    constructor(name : string , age : number ){
+        super();
+        this.age = age ; 
+        this.name = name; 
+    }
 }
 
 
