29a9a974755e805e6a283983ae1f713583793887
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-05-04 10:43:40 +0530

    two city scheduling

---
+++ leetCode_questions/two_city_scheduling.js
@@ -0,0 +1,17 @@
+const parentArray = [[10,20],[30,200],[400,50],[30,20]]
+  
+let sum = 0 ; min ; 
+  parentArray.map(
+    childArray => childArray.map(
+        arrayItem => ({
+            const min =  Math.min(...arrayItem)
+             sum += min  ;
+        }
+             
+        )
+      )
+    )
+  
+
+
+   
