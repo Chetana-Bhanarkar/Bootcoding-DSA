8f16782e55577f11df52b863c15e51efa8c64276
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-05-06 16:20:01 +0530

    calculate sum of digit in string

---
+++ leetCode_questions/calculate_digitSum_of_string.js
@@ -0,0 +1,24 @@
+let  s = "11111222223", k = 3
+
+
+let numbers = Array.from(s);
+// console.log(array);
+
+var digitSum = function(s, k) {
+    let sum = 0 ; 
+    let i = 0 ; 
+    let nums = [] ; 
+
+    for(let j=0 ; j<numbers.length ; j++){
+        sum += parseInt(numbers[j]);
+        if(j >= k-1){
+            nums[i] = sum ; 
+            sum-= numbers[i] ; 
+            i+3 ; 
+        }
+    }
+    console.log(nums);
+};
+
+
+digitSum(s,k)
