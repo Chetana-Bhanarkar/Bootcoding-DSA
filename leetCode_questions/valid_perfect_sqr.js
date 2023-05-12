68a790eedb06f107d69908244a1fb97d717f570c
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-05-12 13:05:45 +0530

    valid perfect square root

--- leetCode_questions/valid_perfect_sqr.js
+++ leetCode_questions/valid_perfect_sqr.js
@@ -1,4 +1,4 @@
-var isPerfectSquare = function(num) {
+var isPerfectSquare = function(n) {
     let root = Math.sqrt(n) ; 
     let res = Math.floor( root) ; 
     let result = res * res == num ? true : false ; 
