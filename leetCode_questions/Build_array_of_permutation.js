e1194dcc2d047344461cb8a751d5a811214ab8df
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-05-31 21:48:46 +0530

    permutation

--- leetCode_questions/Build_array_of_permutation.js
+++ leetCode_questions/Build_array_of_permutation.js
@@ -4,9 +4,7 @@ let nums = [0,2,1,5,3,4];
 var buildArray = function(nums) {
     let a = [];
     for(let i=0;i<nums.length;i++){
-         a[i] = nums[nums[i]];
-
-         
+         a[i] = nums[nums[i]];      
     }
     return a ;
 };
