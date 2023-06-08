e33a1908d6921b2b32ac2985f1d85ca5058faa41
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-06-08 14:32:59 +0530

    constructor in inheritance

---
+++ OOPS/Constructor/constructor_In_Inhertance.ts
@@ -0,0 +1,30 @@
+class animal{
+    constructor(){
+       console.log('animal constructor call.. ');
+       
+    }
+
+}
+
+class dog extends animal{
+    food: string;
+    breed: string;
+
+    constructor(food:string , breed:string){
+        super() ; 
+        this.food = food ; 
+        this.breed = breed ; 
+    }
+
+    getDog(){
+        console.log(this.food , this.breed);
+        
+    };
+    
+}
+
+
+let d = new dog("abc","xyz") ; 
+
+d.getDog();
+
