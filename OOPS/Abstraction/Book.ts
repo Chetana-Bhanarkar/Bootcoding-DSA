442f5a9369134682506b4171ec6358f4d26eff7b
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-06-10 20:48:26 +0530

    abstraction in typescript

--- OOPS/Abstraction/Book.ts
+++ OOPS/Abstraction/Book.ts
@@ -1,38 +1,53 @@
 abstract class Book{
-    constructor(bookname:string){ }
-    bookname():string{
-        return `${this.bookname}`
+    protected bookname :string
+
+    constructor(bookname:string){ 
+        this.bookname = bookname
     }
 
     abstract getReleaseDate():string ; 
-}
 
+    getBookname():string{
+        return `${this.bookname}`
+    }
+}
 
 class TextBook extends Book{
     constructor(bookname:string , private releaseDate : string) {
         super(bookname);
     }
     getReleaseDate(): string {
-        return `${this.releaseDate}`
+        return "TextBook " + this.bookname + " released on " + this.releaseDate ; 
     }
 }
+
 class AudioBook extends Book{
     constructor(bookname:string , private releaseDate : string) {
         super(bookname);
     }
     getReleaseDate(): string {
-        return `${this.releaseDate}`
+        return "AudioBook " + this.bookname + " released on " + this.releaseDate ; 
     }
 }
+
 class EBook extends Book{
     constructor(bookname:string , private releaseDate : string) {
         super(bookname);
     }
     getReleaseDate(): string {
-        return `${this.releaseDate}`
+        return "EBook " + this.bookname + " released on " + this.releaseDate ; 
     }
 }
 
 
 let tbook = new TextBook('Alchemist','10-July-1998') 
-console.log(tbook);
+console.log(tbook.getReleaseDate());
+
+let Abook = new AudioBook('Alchemist','19-April-2001')
+console.log(Abook.getReleaseDate());
+
+let Ebook = new EBook('Alchemist','18-Dec-2010')
+console.log(Ebook.getReleaseDate());
+
+
+
