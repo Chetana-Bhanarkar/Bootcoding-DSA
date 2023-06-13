b71c08db9cf37a8f19f83cc7c3788f046f8dc2c2
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-06-13 12:58:09 +0530

    audiobook ts

---
+++ OOPS/Abstraction/book/audiobook.ts
@@ -0,0 +1,12 @@
+import { Book } from "./Book";
+
+class AudioBook extends Book{
+    constructor(bookname:string , private releaseDate : string) {
+        super(bookname);
+    }
+    getReleaseDate(): string {
+        return "AudioBook " + this.bookname + " released on " + this.releaseDate ; 
+    }
+}
+
+export default AudioBook ; 
