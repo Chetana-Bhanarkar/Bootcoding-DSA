8099581d8944ff95592e6f9901a45ee0f5fe18a9
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-06-23 19:45:53 +0530

    boy ts

---
+++ OOPS/Polymorphism/overriding/Boy/boy.ts
@@ -0,0 +1,11 @@
+export class Boy{
+    name : string ; 
+    constructor(name:string){
+        this.name = name ; 
+    }
+
+    about(){
+        console.log(this.name + " is an intelligent boy");
+        
+    }
+}
