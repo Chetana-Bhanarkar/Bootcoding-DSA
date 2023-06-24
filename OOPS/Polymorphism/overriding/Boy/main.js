ae45c1ed4ae47d771412215168f6582240576fb3
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-06-24 17:59:08 +0530

    main js

---
+++ OOPS/Polymorphism/overriding/Boy/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var student_1 = require("./student");
+var student = new student_1.Student(95, 5402, "Chetana");
+student.getInfo();
+student.about();
