356ab531066cb3e020cd553b0e30c670a85df21c
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-06-26 17:19:20 +0530

    Women js

---
+++ OOPS/Polymorphism/overriding/Women/Women.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Women = void 0;
+var Women = /** @class */ (function () {
+    function Women() {
+        this.name = "maria";
+    }
+    Women.prototype.about = function () {
+        return ("she is ".concat(this.name));
+    };
+    return Women;
+}());
+exports.Women = Women;
