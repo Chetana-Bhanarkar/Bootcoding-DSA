0bd69a94b828137d8a8625b33cd18d9916189f4f
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-06-30 19:51:10 +0530

    example 1 of singleton design pattern

---
+++ Design patterns/Singleton design pattern/example1.ts
@@ -0,0 +1,30 @@
+class Singleton{
+    private static singleton: Singleton;
+    private logger : string[] = []; 
+    constructor(){}
+
+    public static getSingleton(): Singleton{
+        if(!Singleton.singleton){
+            Singleton.singleton = new Singleton()
+        }
+        return Singleton.singleton;
+    }
+
+
+    public pushLogger(data:string): void{
+        this.logger.push(data);
+    }
+
+    public getLogger() : string[]{
+        return this.logger
+    }
+}
+
+let singleOne = Singleton.getSingleton() ;
+let singleTwo = Singleton.getSingleton() ;
+
+
+if(singleOne === singleTwo){
+    console.log("Our singleton is working");    
+}
+ 
