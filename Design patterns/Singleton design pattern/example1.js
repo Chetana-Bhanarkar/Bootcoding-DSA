6a17861b28cf36ad5de3573fa1bf13a2f352f652
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-06-30 19:51:50 +0530

    example 1 of singleton design pattern

---
+++ Design patterns/Singleton design pattern/example1.js
@@ -0,0 +1,23 @@
+var Singleton = /** @class */ (function () {
+    function Singleton() {
+        this.logger = [];
+    }
+    Singleton.getSingleton = function () {
+        if (!Singleton.singleton) {
+            Singleton.singleton = new Singleton();
+        }
+        return Singleton.singleton;
+    };
+    Singleton.prototype.pushLogger = function (data) {
+        this.logger.push(data);
+    };
+    Singleton.prototype.getLogger = function () {
+        return this.logger;
+    };
+    return Singleton;
+}());
+var singleOne = Singleton.getSingleton();
+var singleTwo = Singleton.getSingleton();
+if (singleOne === singleTwo) {
+    console.log("Our singleton is working");
+}
