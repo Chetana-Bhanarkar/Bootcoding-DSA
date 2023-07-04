93af6a23824a06e31a74e9ed9384ca80aa026b98
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-07-04 20:37:37 +0530

    singleton example

--- Design patterns/Singleton design pattern/example three/singleton.js
+++ Design patterns/Singleton design pattern/example three/singleton.js
@@ -16,3 +16,10 @@ var Singleton = (function(){
     };
 })();
 
+function run() {
+
+    var instance1 = Singleton.getInstance();
+    var instance2 = Singleton.getInstance();
+
+    console.log("Same instance? " + (instance1 === instance2));
+}
