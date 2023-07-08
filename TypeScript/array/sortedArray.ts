1627f6d0c56492291337380d0f39fd226a2ebe75
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-07-08 18:15:20 +0530

    sorted array

---
+++ TypeScript/array/sortedArray.ts
@@ -0,0 +1,10 @@
+const sortedArray = (arr : number[]):any => {
+    for(let i=0 ; i<arr.length ; i++){
+        if(arr[i] > arr[i+1]){
+            return "not a sorted array"
+        }
+    }
+    return "sorted array in ascending order" ; 
+}
+
+console.log(sortedArray([1,2,4,5]));
