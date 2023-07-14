f6451e99a3709ab1d5e16812990daa15ebd4e0eb
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-07-14 13:27:32 +0530

    Right Angle Pattern

--- Start Patterns/rightAnglePattern1.ts
+++ Start Patterns/rightAnglePattern1.ts
@@ -1,4 +1,10 @@
 const starPattern = (n : number):any => {
     let string = "" ; 
-    
+    for(let i= 1 ; i<=n ; i++){
+        for(let j=1 ; j<=i ; j++){
+            string += "*";
+        }
+        string += "" ; 
+    }
+    return string
 }
