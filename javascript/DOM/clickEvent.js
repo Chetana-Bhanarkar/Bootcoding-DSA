60287e648714cd1777639535852128870a681f94
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-07-20 18:19:48 +0530

    click event

--- javascript/DOM/clickEvent.js
+++ javascript/DOM/clickEvent.js
@@ -1,1 +1,8 @@
 console.log("I am working");
+
+let p = document.querySelector('.para');
+let btn = document.querySelector('.btn');
+
+btn.addEventListener("click",()=>{
+    p.style.color = "red"
+})
