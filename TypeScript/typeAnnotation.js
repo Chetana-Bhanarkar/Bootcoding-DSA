1f54128e8ccf1bc3afe56b718affec1a8ab833b1
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-07-21 11:22:25 +0530

    type annotation

---
+++ TypeScript/typeAnnotation.js
@@ -0,0 +1,5 @@
+//we can declare the variable, function parameter with the proper datatype using the colon(:), that's called Type Annotation.
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(12, 4));
