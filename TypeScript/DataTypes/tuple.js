42f4526d163a03ed95734ecfbb26d2bb6be90324
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-07-21 14:48:54 +0530

    tuple

---
+++ TypeScript/DataTypes/tuple.js
@@ -0,0 +1,3 @@
+// tuple can contain two or more values of different data type.
+var info = [1, "steve"];
+console.log(info);
---
+++ TypeScript/DataTypes/tuple.ts
@@ -0,0 +1,6 @@
+// tuple can contain two or more values of different data type.
+
+
+let info : [number,string] = [1,"steve"];
+
+console.log(info);
