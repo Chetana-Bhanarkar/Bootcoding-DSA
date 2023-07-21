2833cd1d9ca5fd4ef7913e3642d507f6b9a649b4
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-07-21 12:11:32 +0530

    Boolean

---
+++ TypeScript/DataTypes/Boolean.js
@@ -0,0 +1,5 @@
+//Boolean will contain the value true or false.
+var a = true;
+if (a) {
+    console.log("a is true");
+}
---
+++ TypeScript/DataTypes/Boolean.ts
@@ -0,0 +1,7 @@
+//Boolean will contain the value true or false.
+
+let a : boolean = true;
+
+if(a){
+    console.log("a is true");
+}
