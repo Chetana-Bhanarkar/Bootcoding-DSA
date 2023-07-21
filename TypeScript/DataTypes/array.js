911d061b763660016be97b5002a4171e92636af3
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-07-21 12:47:25 +0530

    array

---
+++ TypeScript/DataTypes/array.js
@@ -0,0 +1,9 @@
+// We can store multiple data in an array
+// Using sqr brackets
+var fruits = ['Apple', 'Orange', 'Banana'];
+console.log(fruits);
+// Using generic 
+var fruit;
+fruit = ['Apple', 'Orange', 'Banana'];
+console.log(fruit);
+// Both will give the same output
---
+++ TypeScript/DataTypes/array.ts
@@ -0,0 +1,21 @@
+// We can store multiple data in an array
+
+
+// Using sqr brackets
+
+
+let fruits : String[] = ['Apple', 'Orange', 'Banana'];
+
+console.log(fruits); 
+
+
+// Using generic 
+
+let fruit : Array<String>;
+fruit = ['Apple', 'Orange', 'Banana'];
+
+console.log(fruit);
+
+
+// Both will give the same output
+
