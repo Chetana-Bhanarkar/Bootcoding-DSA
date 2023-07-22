23661f52c44a0d5ab9dbb6701686eb811547177a
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-07-22 17:15:32 +0530

    promise

---
+++ javascript/Promise.js
@@ -0,0 +1,26 @@
+// syntax Of promise  => 
+
+// let prom = new Promise(function(resolve,reject){
+//     if(true){
+//         resolve()
+//     }
+//     else{
+//         reject()
+//     }
+// })
+
+
+// console.log(prom);
+
+
+let complete = true
+let prom = new Promise((resolve,reject)=>{
+    if(complete){
+        resolve("Fullfiled")
+    }
+    else{
+        reject("reject")
+    }
+})
+
+console.log(prom);
