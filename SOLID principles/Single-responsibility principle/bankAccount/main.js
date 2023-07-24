59b999d48b8faf53a833c9ba0fdd7e22faf3b2aa
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-07-24 11:03:59 +0530

    main js

---
+++ SOLID principles/Single-responsibility principle/bankAccount/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var bankAccount_1 = require("./bankAccount");
+var b = new bankAccount_1.BankAccount();
+console.log(b.deposit(1000));
