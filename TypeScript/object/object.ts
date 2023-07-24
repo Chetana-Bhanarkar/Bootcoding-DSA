5968322016f36873afc2e963828ee094e6b39672
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-07-24 13:23:28 +0530

    object

--- TypeScript/object/object.ts
+++ TypeScript/object/object.ts
@@ -1,9 +1,17 @@
+type objectType = {name:String , age:number}
 
-
-let person : {name:String,age:number} = {
+let person : objectType = {
     name : "Piterson",
     age : 39
 }
 
 
+
+let user : objectType = {
+    name : "steve",
+    age : 44
+}
+
 console.log(person.name);
+console.log(user.age);
+
