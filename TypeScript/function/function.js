340518543c874176b1e1d8bb1b4bcdd71f2ef02a
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-07-28 19:06:35 +0530

    function

--- TypeScript/function/function.js
+++ TypeScript/function/function.js
@@ -3,3 +3,9 @@ var add = function (a, b) {
     return a + b;
 };
 console.log(add(2, 4));
+function test(a) {
+    if (a === undefined)
+        return undefined;
+    return true;
+}
+console.log(test("test"));
