652add135097ff46cc269a7806b2326bf1cf3af9
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-07-31 10:54:12 +0530

    variables

---
+++ javascript/variable.js
@@ -0,0 +1,34 @@
+
+// function variables(){
+//         if(true){
+//             let a = 10 ; 
+//             var b = 20 ;
+
+//             console.log(a,b);
+//         }
+
+//         console.log(a);
+//         console.log(b);
+// }
+
+
+
+const functionScope = () => {
+    var a = 1 ; 
+
+    if(true){
+        var b = 2 ; 
+    }
+
+    console.log(a,b);
+}
+
+
+console.log(a);
+
+
+
+
+
+
+variables()
