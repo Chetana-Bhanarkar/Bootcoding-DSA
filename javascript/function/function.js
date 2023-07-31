1f2fd9e4ac3ccc13a021d7272ef3b56b8baba99d
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-07-31 10:59:56 +0530

    function

---
+++ javascript/function/function.js
@@ -0,0 +1,18 @@
+ 
+//  there are some ways to declare the function ; 
+
+// 1> function decalaration
+function xyz(){
+ // logic
+}
+
+// 2> function expression 
+
+const abc = function(){
+    //logic
+}
+
+// 3> arrow function 
+const pqr = () => {
+    //logic
+}
