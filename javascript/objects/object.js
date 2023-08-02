61eeb1c0b3dc8fd238b11b80d8c648af3d6a260a
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-08-02 11:29:43 +0530

    objects

---
+++ javascript/objects/object.js
@@ -0,0 +1,17 @@
+let person = {
+    name : "john",
+    age : 22,
+    hobbies : ["sleeping","eating","travelling"]
+}
+
+
+
+for(let hob in person.hobbies){
+    console.log(person.hobbies[hob]);
+}
+
+
+
+
+
+
