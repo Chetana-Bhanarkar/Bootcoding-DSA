ee037c6e3c60080c098bcc0bdc33700556c505e2
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-08-03 14:45:55 +0530

    downwards

--- Star Patterns/Star Patterns/downwards.js
+++ Star Patterns/Star Patterns/downwards.js
@@ -1,7 +1,7 @@
 const downwards = (n) => {
     let star = ""
     for(let row=0; row <=n ; row++){
-        for(let col=; col< n-row ; col++){
+        for(let col=1; col<= n-row ; col++){
             star += "*"
         }
         star += "\n"
