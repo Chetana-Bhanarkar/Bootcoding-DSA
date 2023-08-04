8ba53f8c18382efa63896c7b72aee8da424871d5
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-08-04 10:19:54 +0530

    pyramid

---
+++ Star Patterns/Star Patterns/pyramid.js
@@ -0,0 +1,15 @@
+const pyramid = (n) => {
+    let star = "";
+        for(let row=1; row <=n ; row++){
+            for(let spc=1;spc<=n-row ; spc++){
+                star += " ";
+            }
+            for(let col=1;col<=row ; col++){
+                star += "*"
+            }
+            star += "\n"
+        }
+        return star ; 
+}
+
+console.log(pyramid(5));
