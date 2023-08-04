c61404075e700ce26457bea56f54fe54f856b030
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-08-04 11:12:49 +0530

    reverse pyramid

--- Star Patterns/Star Patterns/reversePyramid.js
+++ Star Patterns/Star Patterns/reversePyramid.js
@@ -4,10 +4,13 @@ const reversePyramid = (n) => {
         for(let spc=1 ; spc<row ; spc++){
             star += " "
         }
+        for(let col=1;col<=2*(n-row)-1;col++){
+            star += "*"
+        }
         star += "\n";
     }
     return star ; 
 }
 
 
-console.log(reversePyramid(4));
+console.log(reversePyramid(10));
