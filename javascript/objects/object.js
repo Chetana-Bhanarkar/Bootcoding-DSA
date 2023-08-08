2e333f8e90bd7d49460fef61ebf68a258c7352ae
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-08-08 17:07:25 +0530

    object

--- javascript/objects/object.js
+++ javascript/objects/object.js
@@ -11,6 +11,34 @@ for(let hob in person.hobbies){
 }
 
 
+person.gender = "male" ; 
+console.log(person);
+console.log(person.name);
+console.log(person["name"]);
+
+
+// how to iterate objects
+
+
+
+
+person["gender"] = "male"
+
+
+
+for(let key in person){
+    console.log(person.key);
+}
+let key = "email" ; 
+person[key]  = "e@gmail.com"
+
+console.log(person);
+
+for(let key in person){
+        console.log(`${key} : ${person[key]}`);
+}
+
+
 
 
 
