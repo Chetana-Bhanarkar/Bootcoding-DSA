90c9b9ae2c53ffb0b463a5f44862f3ddce409609
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-08-11 21:04:30 +0530

    diamond pattern

--- Star Patterns/Star Patterns/diamond.js
+++ Star Patterns/Star Patterns/diamond.js
@@ -6,8 +6,12 @@ for (let i = 1; i <= n; i++) {
   for (let j = n; j > i; j--) {
     string += " ";
   }
+  for (let k = 0; k < i * 2 - 1; k++) {
+    string += "*";
+  }
   string += "\n";
- 
+}
+
 return string;
 
 }
