de48ad345c9e4b087726c40efffe92ec61db5de2
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-08-11 21:03:19 +0530

    diamond pattern

---
+++ Star Patterns/Star Patterns/diamond.js
@@ -0,0 +1,15 @@
+const diamond = (n) => {
+let string = "";
+// Upside pyramid
+for (let i = 1; i <= n; i++) {
+
+  for (let j = n; j > i; j--) {
+    string += " ";
+  }
+  string += "\n";
+ 
+return string;
+
+}
+
+console.log(diamond(5));
