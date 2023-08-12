be519517301e09c2e60b1a3476df1a2e92d49dc9
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-08-12 12:23:30 +0530

    open closed Principle

---
+++ SOLID principles/JAVASCRIPT/O.js
@@ -0,0 +1,41 @@
+
+
+class Shape{
+    area(){
+        throw new Error("This method should be overridden by subclasses");
+    }
+}
+
+class Circle extends Shape{
+     radius ; 
+     constructor(radius){
+        super() ; 
+        this.radius = radius ; 
+     }
+
+     area(){
+        return `Area of circle is ${Math.PI * this.radius * this.radius}` ; 
+     }
+}
+
+class Rectangle extends Shape{
+    width;
+    height;
+    constructor(height , width){
+        super()
+       this.width = width;
+       this.height = height;
+    }
+
+    area(){
+        return `Area of Rectangle is ${this.height*this.width}`
+    }
+}
+
+
+
+let c = new Circle(5);
+console.log(c.area());
+
+let r = new Rectangle(3,6);
+console.log(r.area());
