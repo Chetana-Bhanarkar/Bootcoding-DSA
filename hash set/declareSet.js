cbdce6140c7bc689e937e7d0f2f8251b78d48432
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-08-14 16:01:32 +0530

    declare set

--- hash set/declareSet.js
+++ hash set/declareSet.js
@@ -2,6 +2,5 @@ let data  =  new Set([]) ;
 
 data.add('hello');
 data.add('hi') ;
-data.add('hello') ; 
-                                                                                                                                                                         
+data.add('hello') ;                         
 console.log(data);
