cea59bdf3698222d5834bd162222bafc9f2250b6
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-08-17 13:53:02 +0530

    Liskov substitution principle

---
+++ SOLID principles/JAVASCRIPT/L.js
@@ -0,0 +1,8 @@
+// L -> Liskov Substitution Principle
+
+// Def :- If class B is a subtype of A, then we should be able to replace the object of A with B , without breaking the behaviour of program. 
+
+    // SubClass should extends the capability of parent class without narrow it down. 
+
+
+
