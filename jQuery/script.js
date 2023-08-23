749d1e97130f05c1de260744ad8a582215fff74a
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-08-23 11:32:44 +0530

    scripting file

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,16 +1,22 @@
 
-// if we write or use the file of javascript or cdn of jquery then the script file will read only when the document is ready , that's why we not need to write document.ready ;
+// if we write or use the file of javascript or cdn of jquery at the bottom then the script file will read only when the document is ready , that's why we not need to write document.ready ;
 
 $('h1').css('color','red');
 jQuery('button').css('color','blue');
 
 
-// but when we attach the file of javascript or cdn of jquery then the script file will read without waiting the document to be ready , that's why we need to write the document.ready function.
+// but when we attach the file of javascript or cdn of jquery in the head tag then the script file will read without waiting the document to be ready , that's why we need to write the document.ready function.
 
-$(document).ready(()=>{
-    $('h1').css('color','red');
-})
+//like this ->
+
+// $(document).ready(()=>{
+//     $('h1').css('color','red');
+// })
 
 
-//like that
+console.log($("h1").css("font-size"));
+console.log($("h1").css("color"));
+console.log($("h1").css("background-color"));
+ 
+
 
