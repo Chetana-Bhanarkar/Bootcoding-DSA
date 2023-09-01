a4924b7fb41b5ab624a833dd659cbd4d6fe5de01
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-09-01 16:07:05 +0530

    form js

---
+++ javascript/form.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+
+button.addEventListener('click',()=>{
+    document.write(input.value);
+    console.log(input.value);
+})
