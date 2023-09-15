d5cbf32adb6b7d056abbd228de8d2c38f37e6c7d
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-09-15 15:47:47 +0530

    singleton

--- Design patterns/singleton.ts
+++ Design patterns/singleton.ts
@@ -1,30 +1,21 @@
+
 class Singleton {
 
-    private static instance: Singleton | null = null
+    private static instance: { name: string; age: number; score: number; };
 
     public constructor() { }
-
-    
-
-    public static getInstance():Singleton{
-        if(Singleton.instance === null){
-           Singleton.instance = new Singleton()
-        }
-        return Singleton.instance;
-    }
 
     createInstance() {
         return { name: 'John', age: 18, score: Math.floor(Math.random() * 100) }
     }
+    getInstance(){
+        if(!Singleton.instance){
+           Singleton.instance = this.createInstance()
+        }
+        return Singleton.instance;
+    }
 }
 
-const instance1 = new Singleton() ;
-const instance2 = new Singleton() ;
-
-
-console.log(instance1, instance2);
-
-
-console.log(instance1.createInstance());
-console.log(instance2.createInstance());
-
+const instance1 = new Singleton() 
+const instance2 = new Singleton() 
+console.log(instance1.getInstance(),instance2.getInstance());
